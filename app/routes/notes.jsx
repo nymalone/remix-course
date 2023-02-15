fd32138e5cdbd6c2e921fbf9599c1dc9da78cc65
nymalone <malone.nykolle@gmail.com>
2023-02-15 21:12:06 +0000

    new component created - wip: loader function

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,17 +1,34 @@
-import NewNote, { links as NewNoteLinks } from '~/components/NewNote'
+import { redirect } from '@remix-run/node'
+import NewNote, { links as newNoteLinks } from '~/components/NewNote'
+import NoteList, { links as noteListLinks } from '../components/NoteList'
+import { getStoredNotes, storeNotes } from '../data/notes'
 
 export default function NotesPage() {
   return (
     <main>
       <NewNote />
+      <NoteList />
     </main>
   )
 }
 
-export function action() {
+export function loader() {}
+
+export async function action({ request }) {
   // whatever I put here will run on the server, not in the browser
+  const formData = await request.formData() // get the information from the form
+  const noteData = Object.fromEntries(formData) // this will get the entries from the inputs in the form
+
+  //Add validation...
+
+  const existingNotes = await getStoredNotes() // store notes
+  noteData.id = new Date().toISOString()
+  const updatedNotes = existingNotes.concat(noteData)
+  await storeNotes(updatedNotes)
+
+  return redirect('/notes') // redirect user
 }
 
 export function links() {
-  return [...NewNoteLinks()] // surfacing styles
+  return [...newNoteLinks(), ...noteListLinks()] // surfacing styles
 }
